f7884bf03146bbde5a66e7ef317c1bb62181ad36
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-04-26 18:49:24 +0200

    updated included units files paths

--- src/SwapKeysGenerator.dpr
+++ src/SwapKeysGenerator.dpr
@@ -5,10 +5,11 @@ uses
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
   uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule},
-  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
-  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas';
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
+  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas',
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm};
 
 {$R *.res}
 
