4a46bfc0256726722cfbecd1e88ebf03de8649d5
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-05 22:06:51 +0200

    added header copyright comments

--- src/fMain.pas
+++ src/fMain.pas
@@ -1,3 +1,42 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Swap Keys Generator
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Generator of reversible 256-byte blocks for encrypting data by exchanging
+/// values. Each byte in the final set is represented only once.
+///
+/// If you're developing under Delphi or Pascal in general, you can use these blocks with the TOlfCryptDecrypt.SwapCrypt and TOlfCryptDecrypt.SwapDecrypt functions in the Olf.RTL.CryptDecrypt unit from https://github.com/DeveloppeurPascal/librairies/
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://swapkeysgenerator.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Swap-Keys-Generator
+///
+/// ***************************************************************************
+/// File last update : 2024-08-05T21:27:30.669+02:00
+/// Signature : ef556aecb139e8e821a6f95c7d715deaa992f3a9
+/// ***************************************************************************
+/// </summary>
+
 unit fMain;
 
 interface
